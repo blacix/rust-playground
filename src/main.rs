b9496bc80528ffc5470c97a4c78ca5e5b83824f0
Laszlo Bartok <blacix@hotmail.com>
2022-11-24 21:41:32 +0100

    match expressions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::time::Duration;
 use std::thread;
 use std::sync::mpsc::{channel, Receiver};
+use std::cmp::Ordering;
 
 use serialport::SerialPort;
 
@@ -52,7 +53,36 @@ fn serial_thread_function(mut port: Box<dyn SerialPort>, channel: Receiver<Threa
     }
 }
 
+fn compare_stuff() {
+    let x = 1;
+    let x_str = "1".to_string();
+    match x.to_string().cmp(&x_str) {
+        Ordering::Less => println!("less"),
+        Ordering::Greater => println!("greater"),
+        Ordering::Equal => println!("equal"),
+    }
+
+    match x.to_string().cmp(&x_str) {
+        Ordering::Less | Ordering::Greater => println!("not equal"),
+        Ordering::Equal => println!("equal"),
+    }
+
+    let number = 13;
+    match number {
+        // Match a single value
+        1 => println!("One!"),
+        // Match several values
+        2 | 3 | 5 | 7 | 11 | 13 => println!("This is a prime"),
+        // Match an inclusive range
+        13..=19 => println!("A teen"),
+        // Handle the rest of cases
+        _ => println!("Ain't special"),
+        // TODO ^ Try commenting out this catch-all arm
+    }
+}
+
 fn main() {
+    compare_stuff();
     // list available ports
     let ports = serialport::available_ports().expect("No ports found!");
     let mut port_count = 0;
