bfc469b03afa4be822402a7d5a2d89cc352ef6c6
Laszlo Bartok <blacix@hotmail.com>
2022-11-24 17:14:56 +0100

    thread with channel messages

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,96 @@
 use std::time::Duration;
 use std::thread;
+use std::sync::mpsc::{channel, Receiver};
 
-fn parse_v1(data: Vec<u8>) -> String {
-    let parsed_string = String::from_utf8_lossy(&data);
-    return parsed_string.to_string();
+use serialport::SerialPort;
+
+enum ThreadMessage {
+    NoOp,
+    Exit,
 }
 
-fn parse_v2(data: Vec<u8>) -> String {
-    let parsed_string = match String::from_utf8(data) {
-        Ok(data)  => data,
-        // Err(e) => panic!("Problem parsing data: {:?}", e), // will crash the app
-        // Err(e) => "".to_string(), // return empty string
-        Err(e) => e.to_string(), // priont the error message
-    };
-    return parsed_string;
-}
+// fn bytes_to_string_v2(data: Vec<u8>) -> String {
+//     let parsed_string = String::from_utf8_lossy(&data);
+//     return parsed_string.to_string();
+// }
 
-fn parse_v3(data: Vec<u8>) -> String {
+// fn bytes_to_string_v3(data: Vec<u8>) -> String {
+//     let parsed_string = match String::from_utf8(data) {
+//         Ok(data)  => data,
+//         // Err(e) => panic!("Problem parsing data: {:?}", e), // will crash the app
+//         // Err(e) => "".to_string(), // return empty string
+//         Err(e) => e.to_string(), // priont the error message
+//     };
+//     return parsed_string;
+// }
+
+fn bytes_to_string(data: Vec<u8>) -> String {
     let parsed_string = String::from_utf8(data).unwrap_or("could not parse data".to_string());
     return parsed_string;
 }
 
-fn thread_function(mut port: Box<dyn SerialPort> ) {
+fn serial_thread_function(mut port: Box<dyn SerialPort>, receiver: Receiver<ThreadMessage> ) {
     let mut serial_buf: Vec<u8> = vec![0; 32];
-    while true {
-        // let read_len = port.read(serial_buf.as_mut_slice()).unwrap_or(0);
-        // if read_len != 0 {
-        //     println!("len: {}", read_len);
-        //     let string_read = parse_v3(serial_buf[0..read_len].to_vec());
-        //     println!("data: {}", string_read);
-        // }
-
+    let mut running = true;
+    while running {
         if let Ok(read_len) = port.read(serial_buf.as_mut_slice()) {
-            println!("len: {}", read_len);
-            let string_read = parse_v3(serial_buf[0..read_len].to_vec());
-            println!("data: {}", string_read);
+            // println!("len: {}", read_len);
+            let string_read = bytes_to_string(serial_buf[0..read_len].to_vec());
+            print!("{string_read}");
         }
 
-        thread::sleep(Duration::from_millis(10));
+        let message_received = receiver.try_recv().unwrap_or(ThreadMessage::NoOp);
+        if matches!(message_received, ThreadMessage::Exit) {
+            running = false;
+        } else {
+            thread::sleep(Duration::from_millis(10));
+        }
     }
 }
 
 fn main() {
+    // list available ports
     let ports = serialport::available_ports().expect("No ports found!");
     let mut cnt = 1;
+    // reference must be used on ports because iterator changes the collection
+    // and ports.get won't compile
     for p in &ports {
         println!("{} - {}", cnt, p.port_name);
         cnt += 1;
     }
 
+
+    // let the user select a port
     println!("pick a port:");
 
     let mut user_input = String::new();
     std::io::stdin().read_line(&mut user_input).expect("cannot read input");
     let port_index = match user_input.trim().parse::<usize>() {
-        Ok(i)  => i,
+        Ok(i)  => i - 1,
         Err(e) => panic!("Problem parsing data: {:?}", e.to_string())
     };
-
-    let selected_port_info = &ports[port_index];
+    
+    let selected_port_info = ports.get(port_index).unwrap();
     println!("selected port: {}", selected_port_info.port_name.to_string());
 
+
+    // open selected serial port
     let port = serialport::new(selected_port_info.port_name.to_string(), 115200)
     .timeout(Duration::from_millis(10))
     .open().expect("Failed to open port");
 
     println!("port {} open", selected_port_info.port_name.to_string());
+    println!("press enter to exit");
 
 
+    // starting thread to read serial port
+    let (sender, receiver) = channel();
     let handle = thread::spawn(|| {
-        thread_function(port);
+        serial_thread_function(port, receiver);
     });
 
+    // reading user input
+    std::io::stdin().read_line(&mut user_input).expect("cannot read input");
+    sender.send(ThreadMessage::Exit).unwrap();
     handle.join().unwrap();
 }
