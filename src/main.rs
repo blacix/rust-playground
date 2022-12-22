977644461c6790e2718290faac0db07fd5908512
Laszlo Bartok <blacix@hotmail.com>
2022-12-22 22:20:37 +0100

    bluez stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 mod serial_io;
+mod ble;
 
+// fn main() {
+//     serial_io::main_serial_io();
+// }
 
-fn main() {
-    serial_io::main_serial_io();
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> bluer::Result<()> {
+    ble::ble_main().await
 }
